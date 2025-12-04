2f36fae5955ec6dade861f1a8f2f965aba954133
Dj-Viking <anders.swedishviking@gmail.com>
2025-12-03 21:28:22 -0500

    solve part 2 day 1

--- day1/day1.ps1
+++ day1/day1.ps1
@@ -46,3 +46,52 @@ foreach ($line in $input) {
 # 583 is too low
 
 write-host "part 1 $timescrossedzerogoingleft";
+
+$dial = 50;
+
+$timescrossedzero = 0;
+
+# part 2 check every movement tick if we crossed
+# zero
+foreach ($line in $input) {
+	[string]$l = $line;
+	$direction = $l[0]
+	
+	$dist = [int]($l[1..$l.length] -join "");
+
+	# write-host "direction $direction"
+	# write-host "distance from current $dist"
+
+	# past 99 starts over at 0
+	# past 0 starts over at 99
+	switch ($direction) {
+		"L" { 
+			# toward lower numbers
+			for ($i = 0; $i -lt $dist; $i++) {
+				$dial -= 1;
+				if ($dial -eq 0) {
+					$timescrossedzero++;	
+				}
+				while ($dial -lt 0) {
+					$dial += 100;
+				}
+			}
+		}
+		"R" { 
+			# toward higher numbers
+			for ($i = 0; $i -lt $dist; $i++) {
+				$dial = ($dial + 1) % 100;
+				if ($dial -eq 0) {
+					$timescrossedzero++;
+				}
+
+			}
+		}
+	}
+
+	# write-host "dial now $dial"
+}
+
+
+# 2300 is too low
+write-host "part 2 $timescrossedzero";
