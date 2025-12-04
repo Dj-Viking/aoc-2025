ab70b2ed90b2d9d2118c3dd1476d4a366212bbc2
Dj-Viking <anders.swedishviking@gmail.com>
2025-12-03 21:03:52 -0500

    solve part 1 day 1

--- day1/day1.ps1
+++ day1/day1.ps1
@@ -2,8 +2,49 @@ param(
 	[string]$file = "sample"
 )
 
+$dial = 50;
+
 $input = get-content -path $file;
 
+$timescrossedzerogoingleft = 0;
+
 foreach ($line in $input) {
-	write-host $line
+	[string]$l = $line;
+	$direction = $l[0]
+	
+	$dist = [int]($l[1..$l.length] -join "");
+
+	# write-host "direction $direction"
+	# write-host "distance from current $dist"
+
+	# past 99 starts over at 0
+	# past 0 starts over at 99
+	switch ($direction) {
+		"L" { 
+			# toward lower numbers
+			$dial -= $dist;
+			while ($dial -lt 0) {
+				$dial += 100;
+			}
+		}
+		"R" { 
+			# toward higher numbers
+			$dial = ($dial + $dist) % 100
+		}
+	}
+
+	# write-host "dial now $dial"
+
+	if ($dial -eq 0) {
+		$timescrossedzerogoingleft++;
+	}
+
+	$prevdirection = $direction
+
 }
+
+# 24 was wrong!!
+# 19 was wrong!!
+# 583 is too low
+
+write-host "part 1 $timescrossedzerogoingleft";
