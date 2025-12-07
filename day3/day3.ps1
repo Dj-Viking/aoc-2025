fca0861f6e70ca17fa966b4df00be367fad9525d
Dj-Viking <anders.swedishviking@gmail.com>
2025-12-07 10:42:42 -0500

    solve part1 day3

--- day3/day3.ps1
+++ day3/day3.ps1
@@ -27,64 +27,31 @@ foreach($line in $in) {
 #	producing 89 jolts.
 	#>
 	$bats = @() 
-	:connecting for ($i = 0; $i -lt $joltages.length; $i++) 
+	$first_idx = 0;
+	$second_idx = 1;
+	:firstpass for ($i = 0; $i -lt $joltages.length - 1; $i++) 
 	{
 		$bat = $joltages[$i];
-		if ($bats.length -eq 0) 
-		{
-			$bats += $bat;
-			continue connecting;
+		if ($bat -gt $joltages[$first_idx]) {
+			$first_idx = $i
 		}
-
-		if ($bats.length -eq 1 `
-		-and $bat -gt $bats[0]) 
-		{
-			$bats[0] = $bat;
-			continue connecting;
-		} 
-
-		if ($bats.length -eq 1 `
-		-and $bat -lt $bats[0]) 
-		{
-			$bats += $bat;
-			continue connecting;
-		}
-
-		#  4       4(reset)  |  7 and not the end!
-		#  7       ( )       |  8
-		#  7        8
-		if ($bats.length -eq 2 `
-		-and $bats[0] -lt $bat `
-		-and $bats[0] -le $bats[1] `
-		-and $i -ne $joltages.length - 1) 
-		{
-			$bats = @($bat, 0)
-			continue connecting;
-		}
-
-		if ($bats.length -eq 2 `
-		-and $bats[1] -lt $bat `
-		-and $bats[0] -ge $bats[1]) 
-		{
-			$bats[1] = $bat
-			continue connecting;
-		}
-		<#
-        818181911112111
-		at the end?
-		#>
-		if ($bats.length -eq 2 `
-		-and $bats[1] -lt $bat `
-		-and $bats[0] -ge $bats[1] `
-		-and $i -eq $joltages.length - 1) 
-		{
-			$bats[1] = $bat;
-			continue connecting;
+		
+	}
+	$second_idx = $first_idx + 1;
+	:secondpass for ($i = $first_idx + 1; $i -lt $joltages.length; $i++) 
+	{
+		$bat = $joltages[$i];
+		if ($bat -gt $joltages[$second_idx]) {
+			$second_idx = $i
 		}
 		
 	}
 
-	$maxjts += [int]($($bats -join ""))
+	$jt = $($joltages[$first_idx] * 10 
+		+ $joltages[$second_idx]
+	)
+
+	$maxjts += $jt
 
 	write-host "=====maxjts"
 	write-host $maxjts
