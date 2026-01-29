3eb5974ca942e3f9568cc575bb3186008577fe2f
Dj-Viking <anders.swedishviking@gmail.com>
2026-01-28 20:21:54 -0500

    solve day5 part2

--- day5/day5.ps1
+++ day5/day5.ps1
@@ -30,10 +30,10 @@ foreach ($line in $in) {
 
 	#looking at single ids
 	if ($is_ranges -eq $false) {
-		$num = [int64]$line;
+		$id = [int64]$line;
 		:range foreach ($range in $ranges) {
-			if ($num -ge $range[0] -and `
-				$num -le $range[1]
+			if ($id -ge $range[0] -and `
+				$id -le $range[1]
 			){
 				$fresh_cnt += 1;
 				break range;
@@ -42,6 +42,58 @@ foreach ($line in $in) {
 		}
 	}
 }
-write-host $fresh_cnt
+write-host "part1: $fresh_cnt"
 
 #part2
+$ranges = [system.collections.arraylist]@();
+# write-host $ranges
+foreach ($line in $in) 
+{
+	if ($line -eq [string]::empty) {
+		break;
+	}
+	#looking at all numbers in id ranges
+	$split = $line.split("-");
+	$ranges.add([system.collections.arraylist]@(
+	  [int64]($split[0]), 
+	  [int64]($split[1])
+	)) | out-null;
+}
+[system.collections.arraylist]$sorted = $ranges | sort-object `
+-property @{Expression = {$_[0][0]}}
+
+# write-host "sorted len: $($sorted.count)"
+foreach ($c in $sorted) {
+	# write-host "item1 $($c[0])"
+	# write-host "item2 $($c[1])"
+	# write-host "===="
+}
+
+
+$i = 0;
+while ($i -lt $sorted.count - 1) 
+{
+	if ($sorted[$i][1] -ge $sorted[$i + 1][0]) 
+	{
+		if ($sorted[$i][1] -lt $sorted[$i + 1][1])
+		{
+			$sorted[$i][1] = $sorted[$i + 1][1];
+		}
+		$sorted.remove($sorted[$i + 1]);
+	} else {
+		$i += 1;
+	}
+}
+
+# write-host "processed $($sorted)"
+foreach ($c in $sorted) {
+	# write-host "item1 $($c[0])"
+	# write-host "item2 $($c[1])"
+	# write-host "===="
+}
+
+$result = 0;
+foreach ($r in $sorted) {
+	$result += [int64]($(($r[1] - $r[0]) + 1))
+}
+write-host "part2: $result";
